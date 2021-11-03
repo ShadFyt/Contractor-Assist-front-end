771dc659c18428ed1d80634d380b8edd1c2b148d
ShadFyt <doanryan10@gmail.com>
2021-11-02 22:47:20 -0500

    Job form/contact ui

--- src/components/jobs/addJobForm.jsx
+++ src/components/jobs/addJobForm.jsx
@@ -1,8 +1,113 @@
 import React from "react";
+import { FiPhone } from "react-icons/fi";
+import { FaAddressCard } from "react-icons/fa";
 
 import {
-    FormControl,
-    FormLabel,
-    FormErrorMessage,
-    FormHelperText,
-  } from "@chakra-ui/react"
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Icon,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  HStack,
+  VStack,
+  Button,
+  Center,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Divider,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
+
+const ContactInfo = () => {
+  return (
+    <>
+      <HStack>
+        <FormControl id="firstName">
+          <FormLabel>First Name</FormLabel>
+          <Input type="text" />
+        </FormControl>
+        <FormControl id="lastName">
+          <FormLabel>Last Name</FormLabel>
+          <Input type="text" />
+        </FormControl>
+      </HStack>
+      <FormControl id="email">
+        <FormLabel>Email</FormLabel>
+        <Input type="email" />
+      </FormControl>
+      <HStack>
+        <FormControl id="phone">
+          <InputGroup>
+            <InputLeftElement
+              pointerEvent="none"
+              children={<Icon as={FiPhone} color="gray.600" />}
+            />
+            <Input type="tel" placeholder="Phone Number" />
+          </InputGroup>
+        </FormControl>
+        <FormControl id="address">
+          <InputGroup>
+            <InputLeftElement
+              pointerEvent="none"
+              children={<Icon as={FaAddressCard} color="gray.600" />}
+            />
+            <Input type="text" placeholder="Address" />
+          </InputGroup>
+        </FormControl>
+      </HStack>
+    </>
+  );
+};
+
+const JobInfo = () => {
+  return (
+    <>
+      <Center>
+        <Text fontSize="large">Job Details</Text>
+      </Center>
+    </>
+  );
+};
+
+const JobFormModal = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <Button onClick={onOpen}>Add</Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>
+            <Center>Add New Job</Center>
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <VStack>
+              <ContactInfo />
+              <Divider />
+              <JobInfo />
+            </VStack>
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="green" mr={1}>
+              Submit
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default JobFormModal;
--- src/components/jobs/index.jsx
+++ src/components/jobs/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 import { Box, SimpleGrid } from "@chakra-ui/react";
 import Job from "./JobComponent";
+import JobFormModal from "./addJobForm";
 
 function ListJobs(props) {
-  const jobs = useSelector(state => state.job.listOfJobs)
+  const jobs = useSelector((state) => state.job.listOfJobs);
   return (
     <>
+      <JobFormModal />
       <SimpleGrid columns={{ base: 1, lg: 2 }} spacing={10} m={3}>
         {jobs.map((job) => (
           <Box h="full" w="full" key={job.id}>
