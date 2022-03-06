e71446f83d573df10ec79460e0577870b4de1800
ShadFyt <doanryan10@gmail.com>
2022-03-06 13:36:14 -0600

    fixed getDate func

--- src/components/homePage/home.jsx
+++ src/components/homePage/home.jsx
@@ -1,11 +1,30 @@
-import React from "react";
-import { HStack, Box } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { HStack, Box, Center, FormControl, Input } from "@chakra-ui/react";
 import { ExpenseChart } from "./charts/expenseChart";
 import { ProfitChart } from "./charts/profitChart";
 import { RevenueChart } from "./charts/revenueChart";
 import { TimeSheetTab } from "./weeklyTimeSheet";
 
 const Home = () => {
+  const getDate = () => {
+    let date = new Date();
+    let day = "" + date.getDate();
+    if (day < 10) {
+      day = 0 + day;
+    }
+    let month = "" + (date.getMonth() + 1);
+    if (month < 10) {
+      month = "0" + month;
+    }
+    let year = date.getFullYear();
+    console.log("month: ", month);
+    return `${year}-${month}-${day}`;
+  };
+  const [dateRange, setDateRange] = useState(() => getDate());
+
+  const handleDateChange = (e) => setDateRange(e.target.value);
+
+  console.log(dateRange);
   return (
     <>
       <HStack>
@@ -16,6 +35,20 @@ const Home = () => {
       </HStack>
       <Box border={"1px"} m={2} borderRadius="2xl">
         <TimeSheetTab />
+        <Center>
+          <Box w={"15rem"} boxShadow="base">
+            <FormControl>
+              <Input
+                id="dateRange"
+                name="dateRange"
+                value={dateRange}
+                onChange={handleDateChange}
+                type="date"
+                textAlign={"center"}
+              />
+            </FormControl>
+          </Box>
+        </Center>
       </Box>
     </>
   );
